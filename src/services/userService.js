b972e43635a3f0fe5e6a69b0de8cf9b3189c71fc
thienxa28200022@gmail.com <thienxa2820022@gmail.com>
2021-12-30 16:38:17 +0700

    fix APIs

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,69 +1,78 @@
-
-import db from '../models/index.js'
+import db from '../models/index';
 import bcrypt from 'bcryptjs';
 
+
+
 let handleUserLogin = (email, password) => {
-    return new Promise(async (resolve, reject) =>{
-        try{
+    return new Promise(async (resolve, reject) => {
+        try {
             let userData = {};
-
             let isExist = await checkUserEmail(email);
-            if(isExist){
-                // user already exists
-                // compare password
+            if (isExist) {
+                //user already exist
                 let user = await db.User.findOne({
-                    attributes:['email','roleId','password'],
-                    where: {email:email},
-                    raw :true,
+                    attributes: ['email', 'roleId', 'password'],
+                    where: { email: email },
+                    raw: true,
+
                 });
-                if(user){
-                    // compare password
-                    // brcrypt.compareSync
-                let check = await  bcrypt.compareSync(password, user.password); // false
-                if(check ){
-                    userData.errCode = 0;
-                    userData.errMessage ='Okay';
-                    delete user.password;
-                    userData.user = user;
-                }else{
-                    userData.errCode = 3;
-                    userData.errMessage ='Wrong password';
-                   
-                }
-            } else{
+                if (user) {
+                    //compare password: dùng cách 1 hay cách 2 đều chạy đúng cả =))
+                    // Cách 1: dùng asynchronous (bất đồng bộ)
+                    let check = await bcrypt.compare(password, user.password);
+
+
+                    // Cách 2: dùng synchronous  (đồng bộ)
+                    // let check = bcrypt.compareSync(password, user.password);
+
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.errMessage = 'OK';
+
+                        delete user.password;
+                        userData.user = user;
+                    }
+                    else {
+                        userData.errCode = 3;
+                        userData.errMessage = 'Wrong password';
+                    }
+                } else {
                     userData.errCode = 2;
-                    userData.errMessage = `user not found`;
-              
+                    userData.errMessage = `User not found`;
                 }
-                
-           
-            } else{
-                // return error
+
+            } else {
+                //return error
                 userData.errCode = 1;
-                userData.errMessage = `User not found . Please try new email address.` ;
-      
+                userData.errMessage = `Your's Email isn't exist in our system, plz try other email`
             }
-            resolve(userData);
-        } catch(e){
+            resolve(userData)
+        } catch (e) {
             reject(e);
         }
     })
 }
 
-let checkUserEmail = (userEmail)=>{
-    return new Promise(async (resolve, reject)=>{
-        try{
-            let user = await db.User.findOne({where: {email: userEmail}})
-            if(user){
-                resolve(true);
-            } else{resolve(false)}
-        } catch(e){
-            reject(e);
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { email: userEmail }
+            })
+            if (user) {
+                resolve(true)
+            } else {
+                resolve(false)
+            }
+
+        } catch (e) {
+            reject(e)
         }
     })
 }
+
 
 
 module.exports = {
-    handleUserLogin:handleUserLogin,
+    handleUserLogin: handleUserLogin,
 }
