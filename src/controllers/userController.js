350a3276fd35fd50f6f8ec074c217a35f408ceec
thienxa28200022@gmail.com <thienxa2820022@gmail.com>
2022-01-05 10:37:54 +0700

    api get code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -37,7 +37,7 @@ let handleGetAllUsers = async(req, res) =>{
         })
     }
     let users = await userService.getAllUsers(id);
-    console.log(users);
+    // console.log(users);
 
     return res.status(200).json(
        { errCode:0,
@@ -64,10 +64,26 @@ let handleEditUser = async (req,res) => {
     let message = await userService.updateNewUser(data);
     return res.status(200).json(message);
 }
+let getAllCode = async (req, res) => {
+    try{
+
+        let data  = await userService.getAllCodeService(req.query.type);
+        return res.status(200).json(data);
+    }catch(e){
+        console.log('Get all code error : ',e)
+        return res.status(404).json({
+            errCode: -1,
+            errMessage : 'Error from the server'
+        })
+    }
+}
+
+
 module.exports = {
     handleLogin: handleLogin,
     handleGetAllUsers:handleGetAllUsers,
     handleCreateNewUser:handleCreateNewUser,
     handleEditUser:handleEditUser,
     handleDeleteUser:handleDeleteUser,
+    getAllCode:getAllCode
 }
