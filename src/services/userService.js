f2029649ed3c773a09f1b899851ed766c5dfa90d
thienxa28200022@gmail.com <thienxa2820022@gmail.com>
2022-01-07 20:36:34 +0700

    #61: edit something nodejs

--- src/services/userService.js
+++ src/services/userService.js
@@ -179,8 +179,9 @@ let createNewUser =  async (data) => {
                 lastName:data.lastName,
                 address: data.address,
                 phonenumber: data.phonenumber,
-                gender: data.gender === '1' ? true : false,
+                gender: data.gender ,
                 roleId: data.roleId,
+                positionId:data.positionId
             
             })
 
@@ -227,7 +228,7 @@ let deleteUser = (userId)=>{
 let updateNewUser = (data) => {
     return new Promise( async (resolve, reject) => {
         try{
-            if(!data.id){
+            if(!data.id || !data.roleId || !data.positionId || !data.gender){
                 resolve({
                     errCode:2,
                     errMessage:`Missing required parameter`,
@@ -241,6 +242,9 @@ let updateNewUser = (data) => {
                 user.firstName = data.firstName;
                 user.lastName = data.lastName;
                 user.address = data.address;
+                user.roleId = data.roleId;
+                user.positionId = data.positionId;
+                user.phonenumber = data.phonenumber;
                 await user.save();
 
                 resolve({
