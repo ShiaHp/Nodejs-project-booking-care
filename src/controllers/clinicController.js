969341f2ecc5888afbc300dc4f28220ee97b7862
thienxa28200022@gmail.com <thienxa2820022@gmail.com>
2022-01-24 21:52:53 +0700

    Api lấy danh sách phòng,load động,apit get detail clinic,update phòng khám

--- src/controllers/clinicController.js
+++ src/controllers/clinicController.js
@@ -13,7 +13,36 @@ let createClinic = async (req, res) =>{
     })
 }
 }
+let getAllClinic = async (req, res) =>
+{
+    try{
+        let infor = await clinicService.getAllClinic();
+        return res.status(200).json(infor);
+}catch(e){
+    return res.status(200).json({
+        errCode: -1 ,
+        errMessage: 'Error from server' + e.message,
 
+    })
+}
+}
+
+let getDetailClinicById = async (req, res) =>
+{
+    try{
+        let infor = await clinicService.getDetailClinicById(req.query.id);
+        return res.status(200).json(infor);
+        }catch(e){
+    return res.status(200).json({
+        errCode: -1 ,
+        errMessage: 'Error from server' + e.message,
+
+    })
+}
+}
 module.exports = {
-    createClinic :createClinic
+    createClinic :createClinic ,
+    getAllClinic : getAllClinic, 
+    getDetailClinicById :  getDetailClinicById 
+ 
 }
--- src/services/clinicService.js
+++ src/services/clinicService.js
@@ -32,6 +32,79 @@ let createClinic = (data) =>{
         })
 }
 
+
+let getAllClinic = () =>{
+    return new Promise( async (resolve, reject) => {
+
+        try {
+            let data = await db.Clinic.findAll();
+           
+            if(data && data.length >0){
+                data.map(item => {
+                    item.image = new Buffer(item.image,'base64').toString('binary');
+                    return item
+                })
+                
+            }
+            resolve({
+                errCode : 0,
+                errMessage : 'OK',
+                data
+            })
+        } catch (error) {
+            reject(error)
+        }
+               
+        
+            })
+}
+let getDetailClinicById = (inputId) =>{
+
+
+    return new Promise( async (resolve, reject) => {
+        try {
+            
+            if(!inputId ) {
+                resolve({
+                    errCode : 1,
+                    errMessage : 'Missing required parameter'
+                })
+            } else {   
+                  let data = await db.Clinic.findOne({
+                        where: { id : inputId} ,
+                        attributes : ['descriptionHTML','descriptionMarkdown' , 'name', 'address']
+                    })
+                 
+                  if(data) {
+                      let doctorClinic = [];
+                      
+                      doctorClinic  = await db.Doctor_Infor.findAll({
+                            where: { clinicId :inputId } ,
+                            attributes : ['doctorId']
+                        })
+                     
+                    
+
+
+                    data.doctorClinic=    doctorClinic;
+                        // gán tiếp 
+            
+        }  else data = {};
+            resolve({
+                errCode : 0,
+                errMessage : 'ok',
+                data
+            })
+                   
+        }
+
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
 module.exports = {
-    createClinic :createClinic
+    createClinic :createClinic ,
+    getAllClinic :getAllClinic ,
+    getDetailClinicById  : getDetailClinicById
 }
